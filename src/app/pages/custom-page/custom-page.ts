acc013dce273987e3894f403bee01f30a862e3e6
jhernandez-2020477 <jhernandez-2020477@kinal.edu.gt>
2025-11-17 18:48:36 -0600

    Add styles using pipe

--- src/app/pages/custom-page/custom-page.ts
+++ src/app/pages/custom-page/custom-page.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/hero.data';
+import { CanFlyPipe } from '../../pipes/can-Fly.pipe';
+import { HeroColorPipe } from '../../pipes/heroColor.pipe';
+import { HeroTextColorPipe } from '../../pipes/hero-text-color.pipe';
+import { TitleCasePipe } from '@angular/common';
+import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [],
+  imports: [ToggleCasePipe, CanFlyPipe, HeroColorPipe, HeroTextColorPipe, TitleCasePipe, HeroCreatorPipe],
   templateUrl: './custom-page.html',
 })
-export default class CustomPage { }
+export default class CustomPage {
+
+   name = signal('Juan HDZ')
+
+   upperCase = signal(true)
+
+   heroes = signal(heroes)
+
+ }
